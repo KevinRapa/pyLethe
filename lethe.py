44b803af5c75b3abae492b9247b6a2fd5d1c3263
KevinRapa <krap1@umbc.edu>
2019-09-30 22:23:38 -0400

    commit

---
+++ lethe.py
@@ -0,0 +1,8 @@
+import sys, os
+sys.path.append(os.getcwd() + os.sep + "src")
+
+import Main
+
+#        if Player.pos[0] == 4 and Player.pos[2] < 7: # Starts monster if player is in the 
+
+Main.startGame()
